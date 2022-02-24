79bd3f8cf92f33e0ee2ea8a863529f9eeb478d3c
3braheem <ibraahimsiri@gmail.com>
2022-02-23 16:50:08 -0800

    Parsing functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
 pub struct Config {
@@ -9,11 +10,49 @@ pub struct Config {
 impl Config {
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         args.next();
-        let action = args.next().ok_or("No query specified.")?;
+        let action = args.next().ok_or("No command specified.")?;
         let filename = args.next().ok_or("No filename found.")?;
         Ok(Config {
             action,
             filename,
         })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    let results = inln_parse(&config.action, &contents);
+    match config.action.as_str() {
+        "l" => println!("The line count is: {}", results),
+        "w" => println!("The word count is: {}", results),
+        "c" => println!("The character count is: {}", results),
+        _ => {
+            eprintln!("Wrong arguments: Use l for lines, w for words, or c for characters");
+            std::process::exit(1);
+        }
+    }
+    Ok(())
+}
+
+pub fn inln_parse<'a>(action: &str, contents: &'a str) -> usize {
+    match action {
+        "l" => {
+            contents
+                .lines()
+                .count()
+        }
+        "w" => {
+            contents
+                .split_whitespace()
+                .count()
+        }
+        "c" => {
+            contents
+                .replace("\n", "")
+                .replace(" ", "")
+                .chars()
+                .count()
+        }
+        _ => 0
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use inln::Config;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Argument parsing error: {}", err);
+        std::process::exit(1);
+    });
+    if let Err(e) = inln::run(config) {
+        eprintln!("There was an error: {}", e);
+        std::process::exit(1);
+    }
 }
